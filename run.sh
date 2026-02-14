98b1a68a723f4a8767be0f9ecc9518c80fbd8555
Rama Tobbo <ramatobbo5@gmail.com>
2026-02-14 21:19:39 +0200

    Docker production build working

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 # Start R script in background
 
 
-# Start Python app
-gunicorn app:app --bind 0.0.0.0:$PORT --timeout 600 --workers 1
+#!/bin/sh
+gunicorn app:app --bind 0.0.0.0:$PORT --timeout 1200 --workers 1 --threads 2
+
